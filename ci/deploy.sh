02fd22c4d3aad08cc7273f8a86563af03e611a8a
para7008 <rajalingum.patha.consultant@nielsen.com>
2019-01-31 18:11:15 +0530

    Fourth

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,7 +3,7 @@
 export env=$1
 export version=$2
 JAR_NAME=/home/travis/build/Rajaligum/MyTestApp/target/MyTestApp-${version}.jar
-CLASS_NAME="com.exp.MyFirstCIExample"
+CLASS_NAME="com.exp.MainClass"
 
 if [ ${env} = "dev" ] || [ ${env} = "staging" ] || [ ${env} = "prod" ]; then 
     java -cp ${JAR_NAME} ${CLASS_NAME}
